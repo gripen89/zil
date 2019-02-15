fff122a5d83d04498a78e8e70ca337134bad42f8
gripen89 <47283478+gripen89@users.noreply.github.com>
2019-02-16 00:17:01 +0300

    Create reinstall2.sh

---
+++ reinstall2.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+
+cp ~/join/myaddr.txt ~/myaddr.txt
+cp ~/join/mykey.txt ~/mykey.txt
+cp ~/join/constants.xml ~/constants.xml
+sudo docker stop zilliqa
+sudo rm -rf ~/join/
+mkdir ~/join
+cd ~/join
+wget https://mainnet-join.zilliqa.com/configuration.tar.gz
+tar zxvf configuration.tar.gz
+cp ~/myaddr.txt ~/join/myaddr.txt 
+cp ~/mykey.txt ~/join/mykey.txt 
+cp ~/constants.xml ~/join/constants.xml 
+./launch_docker.sh 
